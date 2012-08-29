b418cae5aae713f74a967a41078b1e8d94f3f627
weetabix-su <vovo27_miranemiko@yahoo.co.jp>
2012-08-29 11:39:26 +0800

    Update win32_start.bat
    Added debug console-like cosmetic effect.

--- win32_start.bat
+++ win32_start.bat
@@ -1,1 +1,11 @@
-start for_win32\luaplayer.exe win32.lua
+@ECHO OFF
+color 02
+echo LuaPlayer 0.20 Debug Console
+echo Modified by weetabix
+echo =====================================
+echo.
+for_win32\luaplayer.exe win32.lua
+echo.
+echo [END OF LINE]
+echo.
+pause
